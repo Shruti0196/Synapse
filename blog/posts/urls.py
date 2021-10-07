2197b6657759a355dca8b4f7ee456cedfd052ef9
Shruti0196 <shrutiap2003@gmail.com>
2021-10-07 13:36:18 +0530

    Blog

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
-    path('post/<str:pk>',views.posts,name='post')
+    path('post/<str:pk>',views.posts,name='post'),
+    path('addblog',views.addblog,name='addblog'),
+    path('addblogdisplay',views.addblogdisplay,name='addblogdisplay'),
+    path('deleteblog',views.deleteblog,name='deleteblog'),
+    path('displaydeleteblog',views.displaydeleteblog,name='displaydeleteblog')
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,6 @@
-
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import post
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -10,3 +10,28 @@ def index(request):
 def posts(request,pk):
     posts=post.objects.get(title=pk)
     return render(request,'post.html',{'posts':posts})
+    
+def addblog(request):
+    return render(request,'addblog.html')
+
+def addblogdisplay(request):
+    title=request.GET['title']
+    body=request.GET['description']
+    posts=post.objects.create(title=title,body=body)
+    posts.save();
+    return redirect('index')
+def deleteblog(request):
+    return render(request,'deleteblog.html')
+
+def displaydeleteblog(request):
+    title=request.GET['title']
+    if post.objects.filter(title=title).exists():
+        posts=post.objects.get(title=title)
+        posts.delete()
+        return redirect('index')
+    else:
+        messages.info(request,'Title does not exist')
+        return redirect('deleteblog')
+    
+        
+
